06abd6a6d4e4bbec2a637851a2c10972d48e2cad
Rajz-09 <22010101402@darshan.ac.in>
2025-01-12 01:07:01 +0530

    Packages updated

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ require("dotenv").config(); // Load environment variables
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const Razorpay = require("razorpay");
