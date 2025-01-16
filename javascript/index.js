67996f4b322ea37c4962e7a8eb78afb9f92ffb2a
Rajz-09 <22010101402@darshan.ac.in>
2025-01-17 03:41:17 +0530

    Navbar Updated

--- javascript/index.js
+++ javascript/index.js
@@ -68,7 +68,7 @@ document.getElementById("registerForm").addEventListener("submit", async functio
         const data = await response.json();
         if (response.ok) {
             alert(data.message);
-            window.location.href = "mainContent.html"; // Redirect to login page
+            window.location.href = "mainContent.html"; // Redirect to main page
         } else {
             alert(data.message);
         }
